758db1799ba60239de8b779e63972008dae6669d
Al-Dabbagh Harith <messi4ever500@gmail.com>
2021-11-04 16:01:46 +0300

    Beautify

--- scripts/activetab.js
+++ scripts/activetab.js
@@ -3,10 +3,10 @@ window.addEventListener("load", function(event) {
   var navLinks = document.getElementsByClassName("header__button");
   var currentPage = url[url.length - 1];
   console.log(navLinks.length);
-  for(var i = 0; i < navLinks.length; i++) {
+  for (var i = 0; i < navLinks.length; i++) {
     var lb = navLinks[i].href.split("/");
     console.log(lb[lb.length - 1])
-    if(lb[lb.length - 1] == currentPage) {
+    if (lb[lb.length - 1] == currentPage) {
       navLinks[i].className = "header__button header__button__active";
     }
   }
--- scripts/loadtime.js
+++ scripts/loadtime.js
@@ -1,9 +1,9 @@
 const findLoadTime = (function() {
   document.getElementsByClassName("footer__loadtime")[0].innerHTML = 'Page Load Time: ' +
-  (window.performance.timing.domContentLoadedEventEnd -
-  window.performance.timing.navigationStart) + ' ms';
+    (window.performance.timing.domContentLoadedEventEnd -
+      window.performance.timing.navigationStart) + ' ms';
 });
 
 window.addEventListener('load', (event) => {
-    findLoadTime();
+  findLoadTime();
 });
