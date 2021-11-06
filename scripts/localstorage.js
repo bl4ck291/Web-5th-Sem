d66b86706c4089cef1183321735231b212536184
Al-Dabbagh Harith <messi4ever500@gmail.com>
2021-11-06 14:18:40 +0300

    Finished lab5

---
+++ scripts/localstorage.js
@@ -0,0 +1,40 @@
+export default class LocalStorage {
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
+
+  create(data) {
+    data.token = this.token;
+    this.tasks.push(data);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  update(data) {
+    let index = this.getIndexByToken(data.token);
+    if (index !== -1) {
+      this.tasks[index] = data;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
+
+  delete(data) {
+    let index = this.getIndexByToken(data.token);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
+
+  getIndexByToken(token) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].token === token) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  get token() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+};
