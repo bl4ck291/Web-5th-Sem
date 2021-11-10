5d00397193ab747bf66a356570ee2556cc71cf3c
bl4ck291 <foreveravril520@yahoo.com>
2021-11-10 10:51:08 +0300

    fixed active tab, added page load time to home, and change domLoaded to load

--- scripts/activetab.js
+++ scripts/activetab.js
@@ -1,10 +1,9 @@
-window.addEventListener("DOMContentLoaded", function(event) {
-  var url = document.location.pathname.split("/");
+window.addEventListener("load", function(event) {
+  var url = document.location.pathname;
   var navLinks = document.getElementsByClassName("header__button");
-  var currentPage = url[url.length - 1];
   for (var i = 0; i < navLinks.length; i++) {
-    var lb = navLinks[i].href.split("/");
-    if (lb[lb.length - 1] == currentPage) {
+    var lb = navLinks[i].href.substring(7);
+    if (lb == url) {
       navLinks[i].className = "header__button header__button__active";
     }
   }
